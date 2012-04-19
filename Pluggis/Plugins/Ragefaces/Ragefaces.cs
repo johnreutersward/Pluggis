cf9a7e0c66d1f2f53213cc409f9a8dd12e9f0657
rojters <rojters@gmail.com>
2012-04-19 10:56:58 +0200

    exception handling for ragefaces

--- Pluggis/Plugins/Ragefaces/Ragefaces.cs
+++ Pluggis/Plugins/Ragefaces/Ragefaces.cs
@@ -17,13 +17,25 @@ namespace Pluggis
         {
             this.rageID = rageID;
             WebClient client = new WebClient();
-            string rageSON = client.DownloadString("http://ragefac.es/api/id/" + rageID);
+            try
+            {
+                string rageSON = client.DownloadString("http://ragefac.es/api/id/" + rageID);
+                
+                //This is to get rid of the webcache at the end of the resulting string. 
+                string rageSonNoCache = rageSON.Substring(0, rageSON.LastIndexOf("}") + 1);
 
-            //This is to get rid of the webcache at the end of the resulting string. 
-            string rageSonNoCache = rageSON.Substring(0, rageSON.LastIndexOf("}") + 1);
+                Rageface rage = JsonConvert.DeserializeObject<Rageface>(rageSonNoCache);
+                if (rage.items.Count > 0)
+                {
+                    rageInfo = "Rageface tagged as " + rage.items[0].face_tags + " in category " + rage.items[0].face_category + " (" + rage.items[0].face_views + " views)";
+                }
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("Error on web " + e.Message);
+            }
 
-            Rageface rage = JsonConvert.DeserializeObject<Rageface>(rageSonNoCache);
-            rageInfo = "Rageface tagged as " + rage.items[0].face_tags + " in category " + rage.items[0].face_category + " (" + rage.items[0].face_views + " views)"; 
+             
         }
 
         public class Rageface
